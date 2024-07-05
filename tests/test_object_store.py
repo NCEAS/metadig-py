b76b1101964ca15fb200185ea49892515640a048
Jeanette Clark <jclark@nceas.ucsb.edu>
2024-07-05 11:08:54 -0700

    improve tests and add testing for metadata

--- tests/test_object_store.py
+++ tests/test_object_store.py
@@ -1,16 +1,18 @@
-from hashstore import HashStoreFactory
 import pytest
+import os
+import xml.etree.ElementTree as ET
+from hashstore import HashStoreFactory
 from metadig import StoreManager
+
 
 # Create store and put an object in it
 @pytest.fixture
 def store_dir(tmp_path_factory):
     store_path = tmp_path_factory.mktemp("store")
-    obj_path = tmp_path_factory.mktemp("temp") / "numbers.txt"
+    current_dir = os.path.dirname(__file__)
+    obj_path = os.path.join(current_dir, "testdata", "test-data.csv")
+    meta_path = os.path.join(current_dir, "testdata", "test-pid.xml")
 
-    object_content = ''.join(str(i) for i in range(10))
-    with open(obj_path, "w") as f:
-        f.write(object_content)
     hashstore_factory = HashStoreFactory()
 
     # Create a properties dictionary with the required fields
@@ -28,22 +30,28 @@ def store_dir(tmp_path_factory):
     store = hashstore_factory.get_hashstore(module_name, class_name, properties)
 
     store.store_object("test-pid", str(obj_path))
+    store.store_metadata("test-pid", str(meta_path))
     return str(store_path)
 
 
 def test_object_store(store_dir):
 
     properties = {
-        "store_type": 'HashStore',
+        "store_type": "HashStore",
         "store_path": store_dir,
         "store_depth": 3,
         "store_width": 2,
         "store_algorithm": "SHA-256",
         "store_metadata_namespace": "https://ns.dataone.org/service/types/v2.0#SystemMetadata",
     }
-        
+
     manager = StoreManager(properties)
     obj = manager.get_object("test-pid")
-    #content = obj.read()
-    assert obj is not None
-    #assert content == '0123456789'
+
+    content = obj[0].read(10).decode("utf-8")
+    assert content == "Year,Site,"
+
+    meta = obj[1].read().decode("utf-8")
+    root = ET.fromstring(meta)
+    pid = root.find(".//identifier").text
+    assert pid == "test-pid"
