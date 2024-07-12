c423c58603f8ed05797f5ab451accf06ae84d7e8
Jeanette Clark <jclark@nceas.ucsb.edu>
2024-07-12 13:12:29 -0700

    update testing

--- metadig/__init__.py
+++ metadig/__init__.py
@@ -13,5 +13,6 @@ from .variable import toUnicode
 from .object_store import StoreManager
 from .object_store import MetadataNotFoundError
 from .object_store import ObjectNotFoundError
+from .sysmeta import read_sysmeta_element
 
-__all__ = ['StoreManager', 'getType', 'isResolvable', 'isBlank', 'toUnicode']
+__all__ = ['StoreManager', 'getType', 'isResolvable', 'isBlank', 'toUnicode', 'read_sysmeta_element']
