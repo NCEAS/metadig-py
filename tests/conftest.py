3b670178e95fc3226613ec8a20e07c535cfd06c6
Dou Mok <douming.mok@gmail.com>
2025-02-06 15:40:44 -0800

    Refactor 'conftest.py' fixtures and process to initialize HashStore, and fix affected pytests in 'test_metadata' and 'test_object_store' modules

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,5 @@
+"""This pytest conf file provides fixtures (variables, methods) for metadig pytests."""
+
 import os
 import pytest
 from hashstore import HashStoreFactory
@@ -11,7 +13,7 @@ def init_store_path(tmp_path):
     return hashstore_path
 
 @pytest.fixture(name="hashstore_props")
-def init_props(store_path):
+def init_hashstore_props(store_path):
     """Properties to initialize HashStore."""
     properties = {
         "store_path": store_path,
@@ -22,25 +24,39 @@ def init_props(store_path):
     }
     return properties
 
-# Create store and put an object in it
-@pytest.fixture
-def store_dir(store_path, hashstore_props):
-    current_dir = os.path.dirname(__file__)
-    obj_path = os.path.join(current_dir, "testdata", "test-data.csv")
-    obj2_path = os.path.join(current_dir, "testdata", "test-data-2.csv")
-    meta_path = os.path.join(current_dir, "testdata", "test-pid.xml")
+@pytest.fixture(name="storemanager_props")
+def init_storemanager_props(hashstore_props):
+    """Properties to initialize StoreManager."""
+    # Add 'store_type' to hashstore properties
+    hashstore_props["store_type"] = "HashStore"
+    return hashstore_props
 
+@pytest.fixture(name="store")
+def init_hashstore(hashstore_props):
+    """Properties to initialize HashStore."""
     # Get HashStore from factory
     hashstore_factory = HashStoreFactory()
     module_name = "hashstore.filehashstore"
     class_name = "FileHashStore"
     store = hashstore_factory.get_hashstore(module_name, class_name, hashstore_props)
+    return store
+
+# Create store and put an object in it
+@pytest.fixture
+def init_hashstore_with_test_data(store):
+    """Store two data objects with pids 'test-pid' and 'test-pid-2' and one metadata
+    document with pid 'test-pid'"""
+    current_dir = os.path.dirname(__file__)
+    obj_path = os.path.join(current_dir, "testdata", "test-data.csv")
+    obj2_path = os.path.join(current_dir, "testdata", "test-data-2.csv")
+    meta_path = os.path.join(current_dir, "testdata", "test-pid.xml")
 
     store.store_object("test-pid", str(obj_path))
     store.store_object("test-pid-2", str(obj2_path))
     store.store_metadata("test-pid", str(meta_path))
-    return str(store_path)
+    return True
 
+# TODO: Refactor these fixtures later when run_checks is working, repeated code
 @pytest.fixture(name="sample_check_file_path")
 def init_sample_check_file():
     """Return a sample check file from testdata."""
--- tests/test_metadata.py
+++ tests/test_metadata.py
@@ -5,18 +5,9 @@ from metadig import read_sysmeta_element
 from metadig import find_eml_entity
 
 
-def test_sysmeta_fn(store_dir):
-
-    properties = {
-        "store_type": "HashStore",
-        "store_path": store_dir,
-        "store_depth": 3,
-        "store_width": 2,
-        "store_algorithm": "SHA-256",
-        "store_metadata_namespace": "https://ns.dataone.org/service/types/v2.0#SystemMetadata",
-    }
-
-    manager = StoreManager(properties)
+def test_sysmeta_fn(storemanager_props, init_hashstore_with_test_data):
+    assert init_hashstore_with_test_data
+    manager = StoreManager(storemanager_props)
     obj, sys = manager.get_object("test-pid")
 
     fid = read_sysmeta_element(sys, "formatId")
