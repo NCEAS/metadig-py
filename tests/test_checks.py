5ff4e1317549949e0331fa3b63c2125bde69e039
Dou Mok <douming.mok@gmail.com>
2025-02-12 14:31:11 -0800

    Clean up 'test_checks' module formatting

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -1,4 +1,5 @@
 """Test module for the metadig checks module."""
+
 from metadig import checks
 from metadig.object_store import StoreManager
 import os
@@ -7,7 +8,7 @@ import pytest
 
 def get_test_data_path(file_name):
     """Get the path to a test file in tests/testdata"""
-    test_data_directory = os.path.join(os.path.dirname(__file__), 'testdata')
+    test_data_directory = os.path.join(os.path.dirname(__file__), "testdata")
     return os.path.join(test_data_directory, file_name)
 
 
@@ -26,7 +27,9 @@ def test_run_check(storemanager_props, init_hashstore_with_test_data):
     sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
     sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
 
-    result = checks.run_check(sample_check_file_path, sample_metadata_file_path, check_vars)
+    result = checks.run_check(
+        sample_check_file_path, sample_metadata_file_path, check_vars
+    )
     assert result is not None, "Expected a result from the embedded code."
     assert result["Check Status"] == 0
     assert result["Check Result"] is not None
@@ -48,7 +51,9 @@ def test_run_check_multiple_pids(storemanager_props, init_hashstore_with_test_da
     sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
     sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
 
-    result = checks.run_check(sample_check_file_path, sample_metadata_file_path, check_vars)
+    result = checks.run_check(
+        sample_check_file_path, sample_metadata_file_path, check_vars
+    )
     assert result is not None, "Expected a result from the embedded code."
     assert result["Check Status"] == 0
     assert len(result["Check Result"]) == 2
@@ -107,7 +112,7 @@ def test_run_check_error_multiple_pids_one_success_one_failure(
 def test_get_sysmeta_run_check_vars():
     """Test that we are able to retrieve the expected identifier and member node
     from a given sysmeta document."""
-    path = get_test_data_path('doi:10.18739_A2QJ78081_sysmeta.xml')
+    path = get_test_data_path("doi:10.18739_A2QJ78081_sysmeta.xml")
     sm_rn_vars = checks.get_sysmeta_run_check_vars(path)
     assert sm_rn_vars.get("identifier") == "doi:10.18739/A2QJ78081"
     assert sm_rn_vars.get("authoritative_member_node") == "urn:node:ARCTIC"
@@ -115,13 +120,13 @@ def test_get_sysmeta_run_check_vars():
 
 def test_get_sysmeta_run_check_vars_missing_elements():
     """Test that exception is thrown when we are missing expected elements from a sysmeta doc."""
-    path = get_test_data_path('sysmeta_missing_elements.xml')
+    path = get_test_data_path("sysmeta_missing_elements.xml")
     with pytest.raises(AttributeError):
         _ = checks.get_sysmeta_run_check_vars(path)
 
 
 def test_get_sysmeta_run_check_vars_empty_elements():
-    """Test that exception is thrown when we are missing expected elements from a sysmeta doc."""
-    path = get_test_data_path('sysmeta_empty_elements.xml')
+    """Test that exception is thrown when we an expected elements from a sysmeta doc is empty."""
+    path = get_test_data_path("sysmeta_empty_elements.xml")
     with pytest.raises(ValueError):
         _ = checks.get_sysmeta_run_check_vars(path)
