4dc35d1378aeb9790bb2ff96685b33607e2aceca
Dou Mok <douming.mok@gmail.com>
2025-02-19 09:42:49 -0800

    Fix 'test_checks' module after changes to 'run-check' function, and add new test data file

--- tests/test_checks.py
+++ tests/test_checks.py
@@ -13,100 +13,73 @@ def get_test_data_path(file_name):
 
 
 def test_run_check(storemanager_props, init_hashstore_with_test_data):
-    """Test that the 'run_check' method successfully executes the subprocess and check"""
+    """Test that the 'run_check' method successfully executes"""
     assert init_hashstore_with_test_data
     manager = StoreManager(storemanager_props)
     # Confirm no exception is thrown and object and metadata is in place
     _ = manager.get_object("urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1")
 
     # Now execute 'run_check' by providing it the required args
-    check_vars = {}
-    pid = "urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1"
-    check_vars["dataPids"] = [pid]
-    check_vars["storeConfiguration"] = storemanager_props
     sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
     sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
+    sample_sysmeta_file_path = get_test_data_path("doi:10.18739_A2QJ78081_sysmeta.xml")
 
     result = checks.run_check(
-        sample_check_file_path, sample_metadata_file_path, check_vars
+        sample_check_file_path,
+        sample_metadata_file_path,
+        sample_sysmeta_file_path,
+        storemanager_props,
     )
-    assert result is not None, "Expected a result from the embedded code."
+    assert result is not None
     assert result["Check Status"] == 0
     assert result["Check Result"] is not None
 
 
 def test_run_check_multiple_pids(storemanager_props, init_hashstore_with_test_data):
-    """Test that the 'run_check' method successfully executes the subprocess and check"""
+    """Test that the 'run_check' method successfully executes with multiple pids"""
     assert init_hashstore_with_test_data
     manager = StoreManager(storemanager_props)
     # Confirm no exception is thrown and object and metadata is in place
     _ = manager.get_object("urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1")
 
     # Now execute 'run_check' by providing it the required args
-    check_vars = {}
-    pid = "urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1"
-    pid_two = "test-pid"
-    check_vars["dataPids"] = [pid, pid_two]
-    check_vars["storeConfiguration"] = storemanager_props
     sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
     sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
+    sample_sysmeta_file_path = get_test_data_path("doi:10.18739_A2QJ78081_sysmeta.xml")
 
     result = checks.run_check(
-        sample_check_file_path, sample_metadata_file_path, check_vars
+        sample_check_file_path,
+        sample_metadata_file_path,
+        sample_sysmeta_file_path,
+        storemanager_props,
     )
-    assert result is not None, "Expected a result from the embedded code."
+    assert result is not None
     assert result["Check Status"] == 0
     assert len(result["Check Result"]) == 2
 
 
-def test_run_check_error_missing_pid(storemanager_props, init_hashstore_with_test_data):
-    """Test that the 'run_check' returns failed results successfully"""
+def test_run_check_error_missing_pid_objects(storemanager_props, init_hashstore_with_test_data):
+    """Test that the 'run_check' returns failed results successfully when missing a pid"""
     # Initialize hashstore and confirm it is in working order
     assert init_hashstore_with_test_data
     manager = StoreManager(storemanager_props)
     _ = manager.get_object("urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1")
 
     # Now execute 'run_check' by providing it the required args
-    check_vars = {}
-    pid = "pid.not.found"
-    check_vars["dataPids"] = [pid]
-    check_vars["storeConfiguration"] = storemanager_props
     sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
     sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
+    # The sysmeta below is for another DOI, which will return pids that are not stored
+    sample_sysmeta_file_path = get_test_data_path("doi:10.18739_A2DF6K53C_sysmeta.xml")
 
     result = checks.run_check(
-        sample_check_file_path, sample_metadata_file_path, check_vars
+        sample_check_file_path,
+        sample_metadata_file_path,
+        sample_sysmeta_file_path,
+        storemanager_props,
     )
     assert result is not None
     assert result["Check Status"] == 1
     assert result["Check Result"] is not None
-
-
-def test_run_check_error_multiple_pids_one_success_one_failure(
-    storemanager_props,
-    init_hashstore_with_test_data,
-):
-    """Test that the 'run_check' returns failed results successfully when a pid fails
-    even if another pid succeeds"""
-    # Initialize hashstore and confirm it is in working order
-    assert init_hashstore_with_test_data
-    manager = StoreManager(storemanager_props)
-    _ = manager.get_object("urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1")
-
-    # Now execute 'run_check' by providing it the required args
-    check_vars = {}
-    pid = "urn:uuid:6a7a874a-39b5-4855-85d4-0fdfac795cd1"  # Working pid
-    pid_two = "pid.not.found"  # Failing pid
-    check_vars["dataPids"] = [pid, pid_two]
-    check_vars["storeConfiguration"] = storemanager_props
-    sample_check_file_path = get_test_data_path("data.table-text-delimited.glimpse.xml")
-    sample_metadata_file_path = get_test_data_path("doi:10.18739_A2QJ78081.xml")
-
-    result = checks.run_check(
-        sample_check_file_path, sample_metadata_file_path, check_vars
-    )
-    assert result is not None
-    assert result["Check Status"] == 1
 
 
 def test_get_sysmeta_run_check_vars():
